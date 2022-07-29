edb749ed7bbcf5ca51990ca5d64afcafc0c24fca
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-07-29 13:17:43 +0100

    made some refactor

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,14 @@
 const User = require('../models/user.model');
 const { generateOTP, hashPassword } = require('../config/security');
-const { sendEmail } = require('../config/middleware')
+const { sendEmail } = require('../config/middleware');
 const { validationResult } = require('express-validator');
 
-
-const signUpUser = ('/register/', async (req, res) => {
+const signUpUser = (req, res) => {
   // Validates input data at request time
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res
-      .status(400)
-      .json({ message: errors.array() }); // Custom error response if data is invalid
+    return res.status(400).json({ message: errors.array() }); // Custom error response if data is invalid
   }
 
   const data = req.body;
@@ -19,33 +16,27 @@ const signUpUser = ('/register/', async (req, res) => {
   data.token = generateOTP();
   const user = new User(data);
   try {
-    user
-      .save()
-      .then((result) => {
-          return res
-            .status(201)
-            .json({
-              message: 'Registration successful.',
-              data: { 
-                user_id: result._id,
-                email: result.email,
-                verified: result.emailVerified,
-                created_at: result.createdAt
-              }
-            });
-        });
+    user.save().then((result) => {
+      return res.status(201).json({
+        message: 'Registration successful.',
+        data: {
+          user_id: result._id,
+          email: result.email,
+          verified: result.emailVerified,
+          created_at: result.createdAt,
+        },
+      });
+    });
   } catch (err) {
     res
       .status(400)
-      .json({ message: "An Error occurred! Registration unsuccessful"});
+      .json({ message: 'An Error occurred! Registration unsuccessful' });
     console.log(err);
   }
-});
-
+};
 
 const signInUser = (req, res) => {
   res.json({ msg: 'The Login Endpoint is handled here...' });
 };
-
 
 module.exports = { signInUser, signUpUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,35 +1,36 @@
 const mongoose = require('mongoose');
 
-
-const userSchema = new mongoose.Schema({
-    firstName: { 
-        type: String,
-        trim: true
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      trim: true,
     },
-    lastName: { 
-        type: String,
-        trim: true
+    lastName: {
+      type: String,
+      trim: true,
     },
     email: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true,
-        uniqueCaseInsensitive: true
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      uniqueCaseInsensitive: true,
     },
     password: {
-        type: String,
-        trim: true,
-        require: true
+      type: String,
+      trim: true,
+      require: true,
     },
-    emailVerified: { 
-        type: Boolean,
-        default: false
+    emailVerified: {
+      type: Boolean,
+      default: false,
     },
-    token: { type: String }
-},
-{ timestamps: true }
+    token: { type: String },
+  },
+  { timestamps: true }
 );
 
+const User = mongoose.model('User', userSchema);
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = User;
